a5e5cca80c4db39bfb023a72eb9a2fac13a876b1
Ceren <cerengunnes@gmail.com>
2023-12-05 20:40:07 +0300

    Showed characters in the collection view.

--- RickAndMortyApp/RickAndMortyApp/Views/RMCharacterListView.swift
+++ RickAndMortyApp/RickAndMortyApp/Views/RMCharacterListView.swift
@@ -10,7 +10,7 @@ import UIKit
 ///View that handles showing list of character, loader, etc.
 final class RMCharacterListView: UIView {
     
-    private let viewModel = CharacterListViewViewModel()
+    private let viewModel = RMCharacterListViewViewModel()
     
     private let spinner: UIActivityIndicatorView = {
         let spinner = UIActivityIndicatorView(style: .large)
@@ -41,6 +41,7 @@ final class RMCharacterListView: UIView {
         addSubviews(collectionView, spinner)
         addConstraints()
         spinner.startAnimating()
+        viewModel.delegate = self 
         viewModel.fetchCharacters()
         setUpCollectionView()
     }
@@ -66,15 +67,17 @@ final class RMCharacterListView: UIView {
     private func setUpCollectionView() {
         collectionView.dataSource = viewModel
         collectionView.delegate = viewModel
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
-            self.spinner.stopAnimating()
-            self.collectionView.isHidden = false 
-            
-            UIView.animate(withDuration: 0.4) {
-                self.collectionView.alpha = 1
-            }
-        })
     }
 }
 
+extension RMCharacterListView: RMCharacterListViewViewModelDelegate {
+    func didLoadInitialCharacters() {
+        spinner.stopAnimating()
+        collectionView.isHidden = false
+        collectionView.reloadData()
+        UIView.animate(withDuration: 0.4) {
+            self.collectionView.alpha = 1
+        }
+
+    }
+}
